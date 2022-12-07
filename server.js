9b459bfb211f03f2c07ce708108eabedf8df6c0a
namitak8 <namitak@email.unc.edu>
2022-12-07 15:42:20 -0500

    login page

--- server.js
+++ server.js
@@ -1,20 +1,103 @@
-import minimist from 'minimist';
-import express from 'express';
-import path from 'path';
+// import minimist from 'minimist';
+// import express from 'express';
+// import http from 'http';
+// import bcrypt from 'bcrypt';
+// import path from 'path';
+// import bodyParser from 'body-parser';
+//import { userDB } from '../data.js';
+
+// import path from 'path';
+
+// const app = express();
+// app.use(express.urlencoded({extended: true}));
+
+// const args = minimist(process.argv.slice(2));
+// const port = args.port|| 5000;
+
+// app.get('/app/', (req, res) => {
+//     // res.send('200 OK');
+//     res.sendFile(path.resolve("frontend", "login-page.html"));
+// });
+
+// app.get('*', function(req, res){
+//     res.send('404 NOT FOUND');
+// })
+
+// app.listen(port, () => console.log("Server running..."));
+
+const express = require('express');
+const http = require('http');
+const bcrypt = require('bcrypt');
+const path = require("path");
+const bodyParser = require('body-parser');
+const users = require('./data').userDB;
 
 const app = express();
-app.use(express.urlencoded({extended: true}));
+const server = http.createServer(app);
 
-const args = minimist(process.argv.slice(2));
-const port = args.port|| 5000;
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'./frontend')));
 
-app.get('/app/', (req, res) => {
-    // res.send('200 OK');
-    res.sendFile(path.resolve("frontend", "index.html"));
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'./frontend/index.html'));
 });
 
-app.get('*', function(req, res){
-    res.send('404 NOT FOUND');
-})
 
-app.listen(port, () => console.log("Server running..."));
+app.post('/register', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (!foundUser) {
+    
+            let hashPassword = await bcrypt.hash(req.body.password, 10);
+    
+            let newUser = {
+                id: Date.now(),
+                username: req.body.username,
+                email: req.body.email,
+                password: hashPassword,
+            };
+            users.push(newUser);
+            console.log('User list', users);
+    
+            res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
+        } else {
+            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
+});
+
+app.post('/login', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (foundUser) {
+    
+            let submittedPass = req.body.password; 
+            let storedPass = foundUser.password; 
+    
+            const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+            if (passwordMatch) {
+                let usrname = foundUser.username;
+                res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+            } else {
+                res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
+            }
+        }
+        else {
+    
+            let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+            await bcrypt.compare(req.body.password, fakePass);
+    
+            res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
+});
+
+
+server.listen(3000, function(){
+    console.log("server is listening on port: 3000");
+});
