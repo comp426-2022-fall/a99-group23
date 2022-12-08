f9d17a2dfdf6a7f390677bb8d029b827b9111955
namitak8 <namitak@email.unc.edu>
2022-12-08 12:51:26 -0500

    added design details

--- server.js
+++ server.js
@@ -1,30 +1,4 @@
-// import minimist from 'minimist';
-// import express from 'express';
-// import http from 'http';
-// import bcrypt from 'bcrypt';
-// import path from 'path';
-// import bodyParser from 'body-parser';
-//import { userDB } from '../data.js';
-
-// import path from 'path';
-
-// const app = express();
-// app.use(express.urlencoded({extended: true}));
-
-// const args = minimist(process.argv.slice(2));
-// const port = args.port|| 5000;
-
-// app.get('/app/', (req, res) => {
-//     // res.send('200 OK');
-//     res.sendFile(path.resolve("frontend", "login-page.html"));
-// });
-
-// app.get('*', function(req, res){
-//     res.send('404 NOT FOUND');
-// })
-
-// app.listen(port, () => console.log("Server running..."));
-
+const minimist = require('minimist');
 const express = require('express');
 const http = require('http');
 const bcrypt = require('bcrypt');
@@ -34,7 +8,21 @@ const users = require('./data').userDB;
 const database = require('better-sqlite3');
 const app = express();
 const server = http.createServer(app);
-const db = new database('users.db');     // creating a database to store users data 
+const db = new database('users.db'); 
+
+const args = minimist(process.argv.slice(2));
+const port = args.port|| 3000;
+
+// app.get('/app/', (req, res) => {
+//     // res.send('200 OK');
+//     res.sendFile(path.resolve("frontend", "login-page.html"));
+// });
+
+
+
+// app.listen(port, () => console.log("Server running..."));
+
+    // creating a database to store users data 
 
 // creating a user info table 
 const stmt = ` CREATE TABLE IF NOT EXISTS userinfo (
@@ -90,7 +78,7 @@ app.post('/register', async (req, res) => {
 
             res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
         } else {
-            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
+            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a><br><a href='./login.html'>Login</a><br></div>");
         }
     } catch (e) {
         res.send("Error: " + e.message);
@@ -143,6 +131,7 @@ app.post('/login', async (req, res) => {
     }
 });
 
-server.listen(3000, function(){
-    console.log("server is listening on port: 3000");
+
+app.listen(port, function(){
+    console.log("server is listening on port: ", port);
 });
